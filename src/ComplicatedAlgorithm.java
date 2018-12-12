e5473d792c0c646c3d412ec95b28c5bce264abab
Nicklas Nielsen <nicklas.bring.nielsen@gmail.com>
2018-12-12 09:32:37 +0100

    Initial commit

---
+++ src/ComplicatedAlgorithm.java
@@ -0,0 +1,20 @@
+public class ComplicatedAlgorithm {
+    public static void main(String[] args) {
+
+        ExecTimer.start();
+
+
+        for (int i = 0 ; i < 100000 ; i++){
+            if (i % 1000 == 0) System.out.print(".");
+
+            for (int j = 0 ; j < 100000 ; j++){
+                for (int k = 0 ; k < 100000 ; k++){
+
+                }
+            }
+        }
+        ExecTimer.stop();
+    }
+
+}
+
